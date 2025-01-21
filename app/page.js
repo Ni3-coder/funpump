735993b2e2095ebafeaedb227862bce2bd8bc167
Ni3-coder <rautn119@gmail.com>
2025-01-21 17:16:25 +0530

    changes in comments

--- app/page.js
+++ app/page.js
@@ -3,13 +3,11 @@
 import { use, useEffect, useState } from "react"
 import { ethers } from 'ethers'
 
-// Components
 import Header from "./components/Header"
 import List from "./components/List"
 import Token from "./components/Token"
 import Trade from "./components/Trade"
 
-// ABIs & Config
 import Factory from "./abis/Factory.json"
 import config from "./config.json"
 import images from "./images.json"
